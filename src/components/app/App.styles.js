ac916fb47c817a8c3d521b5b1f03eabaf7b96712
Mykyta Kozhemiakin <knayzvladimir26@gmail.com>
2020-12-28 14:59:45 +0100

    initial commit

---
+++ src/components/app/App.styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const AppContainer = styled.div`
+
+`
+export const AppBody = styled.div`
+    
+`
+export const AppHeader = styled.div`
+    
+`
---
+++ src/components/app/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { AppContainer, AppHeader, AppBody } from "./App.styles";
+
+const App: React.FC = () => {
+  return (
+    <AppContainer>
+      <AppHeader>AppHeader</AppHeader>
+      <AppBody>AppBody</AppBody>
+    </AppContainer>
+  );
+};
+
+export default App;
